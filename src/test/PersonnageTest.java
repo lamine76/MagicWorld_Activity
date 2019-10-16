52ac29f051af800fbf970c98ba797dfb0f3243b2
lamine <lamine.messaci@hotmail.fr>
2019-10-16 22:21:37 +0200

    test Force Joueur & Agilité & Intelligence du Personnage

--- src/test/PersonnageTest.java
+++ src/test/PersonnageTest.java
@@ -13,6 +13,7 @@ class PersonnageTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     Scanner sc = new Scanner(System.in);
 
+
     @Test
      public void Ginven_Correct_Messag_When_DisplayNiveauPersonnage_Then_DisplayCorrectProcess(){
         System.setIn(new ByteArrayInputStream("10\n".getBytes()));
@@ -23,5 +24,42 @@ class PersonnageTest {
         assertEquals(output.length() > "Niveau du personnage ?\n".length(), false);
     }
 
-    
+    @Test
+    public void Ginven_Correct_Messag_When_DisplayForcePersonnage_Then_DisplayCorrectProcess(){
+
+        System.setIn(new ByteArrayInputStream("0\n".getBytes()));
+        Guerrier guerrier = new Guerrier("Joueur 1");
+        guerrier.remplireConditionsForce();
+
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals(output.endsWith(""), true);
+        assertEquals(output.length() > "Force du personnage ?\n".length(), false);
+    }
+
+    @Test
+    public void Ginven_Correct_Messag_When_DisplayAgilitéPersonnage_Then_DisplayCorrectProcess(){
+
+        System.setIn(new ByteArrayInputStream("0\n".getBytes()));
+        Guerrier guerrier = new Guerrier("Joueur 1");
+        guerrier.remplireConditionsAgilite();
+
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals(output.endsWith(""), true);
+        assertEquals(output.length() > "Agilité du personnage ?\n".length(), false);
+    }
+
+    @Test
+    public void Ginven_Correct_Messag_When_DisplayIntelligencePersonnage_Then_DisplayCorrectProcess(){
+
+        System.setIn(new ByteArrayInputStream("0\n".getBytes()));
+        Guerrier guerrier = new Guerrier("Joueur 1");
+        guerrier.remplireConditionsIntelligence();
+
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals(output.endsWith(""), true);
+        assertEquals(output.length() > "Intelligence du personnage ?\n".length(), false);
+    }
+
+
+
 }
